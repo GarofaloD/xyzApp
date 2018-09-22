5358faea4c44e9887850760431fd4e2f6a42676a
Daniel Garofalo <danielgarofalo@Daniels-MacBook-Pro-2.local>
2018-09-22 15:41:24 -0500

    second commit: accelerometer set up

--- xyz-machine/xyz-machine/ViewController.swift
+++ xyz-machine/xyz-machine/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreMotion  //This will give us access to the motion instruments
 
 class ViewController: UIViewController {
 
@@ -15,12 +16,25 @@ class ViewController: UIViewController {
     @IBOutlet weak var yLabel: UILabel!
     @IBOutlet weak var zLabel: UILabel!
     
+    var motionManager : CMMotionManager! //Force unwrapping to prebent errors
     
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        motionManager = CMMotionManager() //Instanciate for tehhjis class when it opens
+        
+        //AccelerometerUpdates uses two main values on the HANDLER that need to be passed (check on those by clicking on the valye and Enter): CMaccelerometerData and Error. The idea would be to create a function that will handle those two values in such way that you do not need to manage evrything on the Accelerometer update method. Therefore, we will be creating the updateLabelds function and that will be passed on this side as the handler
+        motionManager.startAccelerometerUpdates(to: .main, withHandler: updateLabels)
+    }
+    
+    //Main handler for the Accelerometerdata
+    func updateLabels(data: CMAccelerometerData?, error: Error?) {
+        //Aslong as there is data coming from the CMAccelerometerdata, create a variable called accelerometer adat. Otherwise EXIT
+        guard let accelerometerData = data else {return}
+        print(accelerometerData)
     }
 
 
