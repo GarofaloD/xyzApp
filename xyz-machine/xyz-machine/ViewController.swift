564ebd7c588f40da710fde7349625da7f9abd369
Daniel Garofalo <danielgarofalo@Daniels-MacBook-Pro-2.local>
2018-09-22 15:10:59 -0500

    first commit: interface done

---
+++ xyz-machine/xyz-machine/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  xyz-machine
+//
+//  Created by Daniel Garofalo on 9/22/18.
+//  Copyright © 2018 Daniel Garofalo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    //Properties
+    @IBOutlet weak var xLabel: UILabel!
+    @IBOutlet weak var yLabel: UILabel!
+    @IBOutlet weak var zLabel: UILabel!
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
